f1519e8249f0f6c21ca93845ac08ff2df2c53df3
Daniel <ecleticdaniel@gmail.com>
2018-10-28 22:47:57 +0100

    Drawn the World Map but i don't fancy it yet will tweak the visuals a little

--- world_population.py
+++ world_population.py
@@ -14,30 +14,40 @@ with open(filename) as f:
     pop_data = json.load(f)
 
 # Print the 2010 population for each country 
+# Plotting a complete Population map
+# Firstly building a dictionary of population data
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict["Year"] == "2010":
         country_name = pop_dict["Country Name"]
         country_population = int(float(pop_dict["Value"]))
         code = get_country_code(country_name)
         if code: 
-            print(code + " : " + str(country_population))
-        else:
-            print("ERROR " + ": " + country_name)
-        # print(country_name + " : " + str(country_population))
+            # print(code + " : " + str(country_population))
+            cc_populations[code] = country_population
+        
 
-# Building our first americas map
-world_map0 = pygal.maps.world.World()
-world_map0.title = "North, Central and South America"
-# North America
-world_map0.add('North America', ['ca', 'mx', 'us'])
-# Central America
-world_map0.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'pa', 'sv'])
-# South America
-world_map0.add('South America', ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'gf','gy', 'pe', 'py', 'sr', 'uy', 've'])
-world_map0.render_to_file('americas.svg')
+# # Building our first americas map
+# world_map0 = pygal.maps.world.World()
+# world_map0.title = "North, Central and South America"
+# # North America
+# world_map0.add('North America', ['ca', 'mx', 'us'])
+# # Central America
+# world_map0.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'pa', 'sv'])
+# # South America
+# world_map0.add('South America', ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'gf','gy', 'pe', 'py', 'sr', 'uy', 've'])
+# world_map0.render_to_file('americas.svg')
 
-# Building our second americas map
-world_map1 = pygal.maps.world.World()
-world_map1.title = 'Populations of Countries in North America'
-world_map1.add('North America', {'ca': 34126000, 'us': 309349000, 'mx': 113423000})
-world_map1.render_to_file('na_populations.svg')
+# # Building our second americas map
+# world_map1 = pygal.maps.world.World()
+# world_map1.title = 'Populations of Countries in North America'
+# world_map1.add('North America', {'ca': 34126000, 'us': 309349000, 'mx': 113423000})
+# world_map1.render_to_file('na_populations.svg')
+
+
+
+
+world_map3 = pygal.maps.world.World()
+world_map3.title = 'World Population in 2010, by Country'
+world_map3.add('2010', cc_populations)
+world_map3.render_to_file('world_population.svg')
