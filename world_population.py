00acb09f562a2e250474380d99a2d7f1de4643f5
Daniel <ecleticdaniel@gmail.com>
2018-10-28 22:27:34 +0100

    built first map of the americas

--- world_population.py
+++ world_population.py
@@ -6,7 +6,7 @@
 # from country_codes import get_country_codes 
 import json
 import pygal 
-import countries
+from country_code import get_country_code
 filename = "population_data.json"
 
 # Load the data into a list
@@ -18,5 +18,20 @@ for pop_dict in pop_data:
     if pop_dict["Year"] == "2010":
         country_name = pop_dict["Country Name"]
         country_population = int(float(pop_dict["Value"]))
-        print(country_name + " : " + str(country_population))
+        code = get_country_code(country_name)
+        if code: 
+            print(code + " : " + str(country_population))
+        else:
+            print("ERROR " + ": " + country_name)
+        # print(country_name + " : " + str(country_population))
 
+# Building our world map
+world_map0 = pygal.maps.world.World()
+world_map0.title = "North, Central and South America"
+# North America
+world_map0.add('North America', ['ca', 'mx', 'us'])
+# Central America
+world_map0.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'pa', 'sv'])
+# South America
+world_map0.add('South America', ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'gf','gy', 'pe', 'py', 'sr', 'uy', 've'])
+world_map0.render_to_file('americas.svg')
