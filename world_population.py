c4386dc68d971999a3e960570f319c09296024ad
Daniel <ecleticdaniel@gmail.com>
2018-10-28 19:56:04 +0100

    Extracting Relevant information from population_data.json

---
+++ world_population.py
@@ -0,0 +1,18 @@
+#examine each country’s name and population
+#only in 2010
+# population_data.json contains world population data from 1960 to 2010
+
+
+import json 
+filename = "population_data.json"
+
+# Load the data into a list
+with open(filename) as f:
+    pop_data = json.load(f)
+
+# Print the 2010 population for each country 
+for pop_dict in pop_data:
+    if pop_dict["Year"] == "2010":
+        country_name = pop_dict["Country Name"]
+        country_population = pop_dict["Value"]
+        print(country_name + " : " + country_population)
