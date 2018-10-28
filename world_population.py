81a4c81cc57fb341ba5155c9542b2da7a0a7496c
Daniel <ecleticdaniel@gmail.com>
2018-10-28 22:35:09 +0100

    Drawn second map of the North

--- world_population.py
+++ world_population.py
@@ -25,7 +25,7 @@ for pop_dict in pop_data:
             print("ERROR " + ": " + country_name)
         # print(country_name + " : " + str(country_population))
 
-# Building our world map
+# Building our first americas map
 world_map0 = pygal.maps.world.World()
 world_map0.title = "North, Central and South America"
 # North America
@@ -35,3 +35,9 @@ world_map0.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'pa', 'sv'])
 # South America
 world_map0.add('South America', ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'gf','gy', 'pe', 'py', 'sr', 'uy', 've'])
 world_map0.render_to_file('americas.svg')
+
+# Building our second americas map
+world_map1 = pygal.maps.world.World()
+world_map1.title = 'Populations of Countries in North America'
+world_map1.add('North America', {'ca': 34126000, 'us': 309349000, 'mx': 113423000})
+world_map1.render_to_file('na_populations.svg')
